11b67c33617a1dacdce01eec35ec8bae6c6d1401
Mazen <mezoahmed2507@gmail.com>
2024-05-11 23:06:59 +0300

    BuildFix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("java")
     //id("io.github.patrick.remapper") version "1.4.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-
 }
 
 group = "io.github.mqzen"
@@ -22,9 +20,9 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.22")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     compileOnly("org.jetbrains:annotations:21.0.1")
-    implementation("net.kyori:adventure-api:4.16.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.2")
-    implementation("net.kyori:adventure-text-minimessage:4.16.0")
+    compileOnly("net.kyori:adventure-api:4.16.0")
+    compileOnly("net.kyori:adventure-platform-bukkit:4.3.2")
+    compileOnly("net.kyori:adventure-text-minimessage:4.16.0")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 
 }
@@ -34,11 +32,6 @@ tasks {
         targetCompatibility = "17"
         options.encoding = "UTF-8"
     }
-
-    shadowJar {
-        archiveFileName.set("${project.name}-${project.version}.jar")
-    }
-
 }
 
 /**tasks.getByName<Test>("test") {
