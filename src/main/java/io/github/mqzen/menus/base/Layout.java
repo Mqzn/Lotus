ba3f1744b5f24e2b9cd09a3e4ac3a10021e3f34f
Mazen <mezoahmed2507@gmail.com>
2024-05-15 18:36:00 +0300

    Added Layouts

--- src/main/java/io/github/mqzen/menus/base/Layout.java
+++ src/main/java/io/github/mqzen/menus/base/Layout.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class Layout {
+public final class Layout {
     @NotNull
     private final Map<Character, Button> mappedButtons;
 
